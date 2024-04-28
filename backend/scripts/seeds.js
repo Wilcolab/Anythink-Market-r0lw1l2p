08f130601801a6763e03e97441d07278a875dfd3
esmoncake <84898847+esmoncake@users.noreply.github.com>
2024-04-28 03:03:54 +0000

    corrections

--- backend/scripts/seeds.js
+++ backend/scripts/seeds.js
@@ -1,5 +1,6 @@
 //TODO: seeds script should come here, so we'll be able to put some data in our local env
 var mongoose = require("mongoose");
+var uniqueValidator = require("mongoose-unique-validator");
 var User = require("../models/User"); // Importa el modelo de usuario
 var Item = require("../models/Item"); // Importa el modelo de producto
 var Comment = require("../models/Comment"); // Importa el modelo de comentario
